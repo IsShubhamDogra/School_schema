f3c1334c1c18aa12063fb5b2e4aa73d94667a3cc
Shubham-choudhary82 <162970905+Shubham-choudhary82@users.noreply.github.com>
2024-07-18 14:45:34 +0530

    added fee form

--- diary/src/app/data/fee-structure/fee-structure.component.ts
+++ diary/src/app/data/fee-structure/fee-structure.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FeeService } from '../../service/fee.service';
 
 @Component({
   selector: 'app-fee-structure',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './fee-structure.component.html',
   styleUrl: './fee-structure.component.css'
 })
 export class FeeStructureComponent {
+  feeForm!: FormGroup;
+  constructor(private fb: FormBuilder,private feeservice:FeeService) {
+    this.feeForm = this.fb.group({
+      class: [''],
+      stream: [''],
+      fee: [''],
+      uniform: [''],
+      tour: ['']
+    });
+  }
+  selectClass(event: Event) {
+    const selectedClass = (event.target as HTMLSelectElement).value;
+    if (selectedClass === '11' || selectedClass === '12') {
+      this.feeForm.addControl('stream', new FormControl(''));
+    } else {
+      this.feeForm.removeControl('stream');
+    }
+  }
 
+  onSubmit() {
+    console.log(this.feeForm.value);
+    const formData = new FormData();
+    formData.append('class', this.feeForm.get('class')?.value);
+    formData.append('stream', this.feeForm.get('stream')?.value);
+    formData.append('fee', this.feeForm.get('fee')?.value);
+    formData.append('uniform', this.feeForm.get('uniform')?.value);
+    formData.append('tour', this.feeForm.get('tour')?.value);
+    this.feeservice.uploadfee(formData).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
---
+++ diary/src/app/service/fee.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeeService {
+  private apiUrl = 'http://localhost:3002'
+
+  constructor(private http:HttpClient) { }
+  uploadfee(data: FormData): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/fee`, data);
+  }
+}
